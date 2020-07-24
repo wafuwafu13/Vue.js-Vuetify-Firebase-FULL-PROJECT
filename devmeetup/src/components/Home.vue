6668901dfe16821e37554e4481c7c157ec5496a0
wafuwafu13 <mariobaske@i.softbank.jp>
2020-07-24 10:44:10 +0900

    6/A_Carousel_on_the_Home_Page

--- devmeetup/src/components/Home.vue
+++ devmeetup/src/components/Home.vue
@@ -1,5 +1,60 @@
 <template>
-  <div>
-    <p>The Home Page</p>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col class="text-center text-sm-right">
+        <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
+      </v-col>
+      <v-col class="text-center text-sm-left">
+        <v-btn large router to="/meetup/new" class="info">Organize Meetup</v-btn>
+      </v-col>
+    </v-row>
+    <v-row class="mt-2">
+      <v-carousel>
+        <v-carousel-item
+          v-for="meetup in meetups"
+          :key="meetup.id"
+          :src="meetup.imageUrl"
+        >
+        <div class="title">
+          {{ meetup.title }}
+        </div>
+        </v-carousel-item>
+      </v-carousel>
+    </v-row>
+    <v-row class="mt-2">
+      <v-col class="text-center">
+        <p>Join our awesome meetups!</p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        meetups: [
+          { imageUrl: 'https://s3-ap-northeast-1.amazonaws.com/tabi-channel/upload_by_admin/newyork_travel_800.jpg', 
+            id: 'jfiorjafaifinrg212',
+            title: 'Meetup in New york' 
+          },
+          { imageUrl: 'https://media.gettyimages.com/photos/eiffel-tower-in-paris-france-picture-id924894324?s=612x612', 
+            id: 'jfeaiufeffag2332',
+            title: 'Meetup in Paris' 
+          },
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background-color: rgba(0,0,0,0.5);
+    color: white;
+    font-size: 2em;
+    padding: 30px;
+  }
+</style>
--- devmeetup/src/plugins/vuetify.js
+++ devmeetup/src/plugins/vuetify.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: colors.red.darken2,
+                accent: colors.red.accent2,
+                secondary: colors.grey.lighten1,
+                info: colors.blue.lighten1,
+                warning: colors.amber.darken2,
+                error: colors.red.accent4,
+                success: colors.green.lighten2,
+            }
+           
+        }
+    }
 })
